b1577c284871c5156cb5bd5f02b40066d2f0b23e
SaurabhSikka <drsaurabhsikka@gmail.com>
2016-09-03 14:00:46 +0200

    create outlets and actions

--- AutolayoutPlayers/ViewController.swift
+++ AutolayoutPlayers/ViewController.swift
@@ -10,16 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
+    
+    
+    @IBAction func loginButton() {
+        
     }
+    
+    @IBAction func securityToggle() {
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    
 }
 
